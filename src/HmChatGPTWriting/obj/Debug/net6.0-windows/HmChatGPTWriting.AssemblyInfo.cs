7a9ecec5d9ac75b28d79a885ab826995fda465bf
komiyamma <komiyama0629@gmail.com>
2024-05-10 01:23:11 +0900

    フォームウィンドウが多重化してしまう問題の修正

--- src/HmChatGPTWriting/obj/Debug/net6.0-windows/HmChatGPTWriting.AssemblyInfo.cs
+++ src/HmChatGPTWriting/obj/Debug/net6.0-windows/HmChatGPTWriting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Akitsugu Komiyama")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.6.3")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.6.3+25b211cd4fe7406192bae28dc80bd17ccb5291bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.6.3+fc00d3496701d14916f4db51b3f7791e8140b02c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmChatGPTWriting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmChatGPTWriting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.6.3")]
